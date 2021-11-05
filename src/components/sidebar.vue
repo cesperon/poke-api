20bafc2899e8f5f6987801f531a9b90712c53e89
cesperon <cesperon4@gmail.com>
2021-11-05 03:58:14 -0700

    updated home page and added sidebar

---
+++ src/components/sidebar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="wrapper">
+    <!-- Sidebar -->
+    <nav id="sidebar">
+      <div class="sidebar-header">
+        <h3>Poke-Search</h3>
+        <img src="../assets/dex.jpg" class="mb-4" />
+      </div>
+
+      <ul class="list-unstyled components">
+        <h4>Regions</h4>
+        <li class="active">
+          <a href="#">Kanto</a>
+        </li>
+        <li>
+          <a href="#">Original Johto</a>
+        </li>
+        <li>
+          <a href="#">Updated Johto</a>
+        </li>
+        <li>
+          <a href="#">Hoenn</a>
+        </li>
+        <li>
+          <a href="#">Original Sinnoh</a>
+        </li>
+        <li>
+          <a href="#">Extended Sinnoh</a>
+        </li>
+        <li>
+          <a href="#">Original Unova</a>
+        </li>
+        <li>
+          <a href="#">Updated Unova</a>
+        </li>
+      </ul>
+    </nav>
+    <!-- Page Content -->
+    <!-- <div id="content">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fas fa-align-left"></i>
+            <span>Toggle Sidebar</span>
+          </button>
+        </div>
+      </nav>
+    </div> -->
+  </div>
+</template>
+
+<script lang="ts" scoped>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Sidebar",
+  components: {},
+});
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  width: 100%;
+}
+
+#sidebar {
+  width: 250px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  z-index: 999;
+  background: #4071cc;
+  color: #fff;
+  transition: all 0.3s;
+}
+a {
+  color: white;
+}
+#sidebar {
+  box-shadow: 8px 8px 6px #888888;
+}
+img {
+  border-radius: 40px;
+  height: 60px;
+  width: 70px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,119 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <img :src="state.pokeImage" alt="" />
+    <sidebar></sidebar>
+    <div class="container">
+      <div class="row">
+        <div class="form-outline col-6 mx-auto mb-4 mt-4">
+          <input
+            v-model="searchText"
+            type="search"
+            id="form1"
+            class="form-control"
+            placeholder="Search by name or pokemon number"
+            aria-label="Search"
+          />
+        </div>
+      </div>
+      <h2>{{ state.region_name }} pokemon</h2>
+      <div v-for="pokemon in filteredList" class="" :key="pokemon.id">
+        <img :src="pokemon.image" />
+        {{ pokemon.name }}
+        {{ pokemon.id }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent, reactive, ref, computed } from "vue";
 import axios from "axios";
 import { onMounted } from "vue";
-import { PokeAPI } from "@/types/pokeApi.interface";
+import { pokeRegion, RegionId } from "@/types/pokeRegion.interface";
+import { pokemonInfo } from "@/types/pokeApi.interface";
+import sidebar from "@/components/sidebar.vue";
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: { sidebar },
   setup() {
-    const hello = "hello";
     //reactive components
+    const state: pokeRegion = reactive({
+      region_api_data: null,
+      region_name: null,
+      region_pokemon: null,
+      version: null,
+    });
 
-    interface myState {
-      poke: PokeAPI | null;
-      pokemon: string | null;
-      pokeImage: string | null;
-    }
-    const state: myState = reactive({
-      poke: null,
-      pokemon: null,
-      pokeImage: null,
+    let searchText = ref("");
+    let pokeList = ref([]);
+    let pokeInfo = ref<pokemonInfo[]>([]);
+
+    const filteredList = computed(() => {
+      return pokeInfo.value.filter((pokemon: pokemonInfo) => {
+        return (
+          pokemon.name.includes(searchText.value) ||
+          pokemon.id == parseInt(searchText.value)
+        );
+      });
     });
 
     //mounted hook
     onMounted(() => {
       getPokemon();
+      console.log("mounted", pokeInfo);
     });
 
-    //mehtods
+    //methods
     const getPokemon = async (): Promise<void> => {
-      const res = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
-      state.poke = res.data;
-      // console.log(res.data);
-      state.pokeImage = res.data.sprites.front_shiny;
-      // console.log(state.pokeImage);
-      state.pokemon = res.data.name;
+      const res = await axios.get("https://pokeapi.co/api/v2/pokedex/9");
+      console.log("res2", res.data);
+
+      state.region_api_data = res.data;
+      state.region_name = res.data.name;
+      state.region_pokemon = res.data.pokemon_entries;
+
+      state.region_pokemon?.forEach((pokemon) => {
+        let name: string = pokemon.pokemon_species.name;
+        getPokemonInfo(name);
+      });
+      console.log("pokeObject", pokeInfo);
+
+      pokeList.value = res.data.pokemon_entries;
+      console.log("pokeList", pokeList);
+      // sortPokemon();
+      pokeInfo.value.sort((a, b) => {
+        return a.id - b.id;
+      });
+      console.log("sortedList", pokeInfo);
+      state.version = res.data.version;
+
+      console.log("kanto", RegionId.updated_johto);
+    };
+    //returns pokemonInfo object with require attributes
+    const getPokemonInfo = async (name: string): Promise<void> => {
+      try {
+        const res = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${name}`
+        );
+        let currentPokemon: pokemonInfo = {
+          id: res.data.id,
+          name: res.data.name,
+          image: res.data.sprites.front_shiny,
+          height: res.data.height,
+          weight: res.data.weight,
+        };
+        pokeInfo.value.push(currentPokemon);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
-    return { hello, state };
+    return { state, searchText, pokeList, filteredList, pokeInfo };
   },
 });
 </script>
+<style>
+input {
+  width: 30px;
+}
+</style>
